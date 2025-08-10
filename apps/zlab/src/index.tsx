6b901d66d9ef373a3908ef91d1ec248a85652409
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2025-08-10 23:05:24 +0000

    feat(core): scaffold ZLab and Zulu apps, add global CoreMemoryButton to UI package and wire to app headers

---
+++ apps/zlab/src/index.tsx
@@ -0,0 +1,10 @@
+import { CoreMemoryButton } from '@zebulon/ui';
+
+export default function ZLabApp() {
+  return (
+    <header>
+      <CoreMemoryButton />
+      {/* ...rest of ZLab UI... */}
+    </header>
+  );
+}
---
+++ apps/zulu/src/index.tsx
@@ -0,0 +1,10 @@
+import { CoreMemoryButton } from '@zebulon/ui';
+
+export default function ZuluApp() {
+  return (
+    <header>
+      <CoreMemoryButton />
+      {/* ...rest of Zulu UI... */}
+    </header>
+  );
+}
